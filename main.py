7c6c99fde75484403a6bb967df8526b6d004ae32
Simon Wenzin <simon.wenzin@gmail.com>
2025-09-21 19:09:27 +0200

    added function for saving to .json file as well as creating a function for loading said .json file

--- main.py
+++ main.py
@@ -1,42 +1,70 @@
+import json
+import os
 import time
 from pyfirmata import Arduino
 
 board = Arduino("/dev/ttyACM0")
 servo_map = {
-    "torso": 2,
-    "shoulder": 3,
-    "elbow": 4,
-    "wrist": 5,
-    "hand": 6,
+    "torso": {"pin": 2, "angle": 90},
+    "shoulder": {"pin": 3, "angle": 90},
+    "elbow": {"pin": 4, "angle": 90},
+    "wrist": {"pin": 5, "angle": 90},
+    "hand": {"pin": 6, "angle": 90},
 }
-
-for pin in servo_map.values():
-    board.servo_config(pin, 544, 2400)
 
 def servo_move(servo, angle):
     """Moves a specific servo a specified angle in degrees."""
-    if servo is None:
+    if servo not in servo_map:
         print(f"invalid servo: '{servo}'")
         return
-    if not (0 <= angle < 180):
+    if not (0 <= angle <= 180):
         print(f"invalid angle: '{angle}'")
         return
-    board.digital[servo_map[servo]].write(angle)
+    board.digital[servo_map[servo]["pin"]].write(angle)
+    servo_map[servo]["angle"] = angle
 
-while True:
-    user_input = input().lower()
-    if user_input == "quit":
-        break
+def save_state(data, filename):
+    state = load_state(filename)
+    state.append(data)
+    with open(filename, "wt") as f:
+        json.dump(state, f)
 
-    elif user_input == "reset":
-        for pin in servo_map.values():
-            board.digital[pin].write(180)
-            time.sleep(0.5)
+def load_state(filename):
+    with open(filename, "rt") as f:
+        return json.load(f)
 
-    try:
-        input_servo, input_angle = user_input.split()
-        input_angle = int(input_angle)
-        servo_move(input_servo, input_angle)
+def delete_file(filename):
+    os.remove(filename)
 
-    except ValueError:
-        print(f"invalid syntax: '{user_input}'")
+def main():
+    for pin in servo_map.values():
+        board.servo_config(pin["pin"], 544, 2400)
+
+    while True:
+        user_input = input("$ ").lower()
+        if user_input == "quit":
+            break
+
+        elif user_input == "reset":
+            for pin in servo_map.values():
+                board.digital[pin["pin"]].write(90)
+                time.sleep(0.5)
+
+        elif user_input == "save":
+            data = [servo_map]
+            save_state(data, "data.json")
+
+        elif user_input == "delete":
+            delete_file("data.json")
+
+        else:
+            try:
+                input_servo, input_angle = user_input.split()
+                input_angle = int(input_angle)
+                servo_move(input_servo, input_angle)
+
+            except ValueError:
+                print(f"invalid syntax: '{user_input}'")
+
+if __name__ == "__main__":
+    main()
