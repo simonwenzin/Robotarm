6a134e6db2cec4d0b28ba199e82b278f42510f56
Simon Wenzin <simon.wenzin@gmail.com>
2025-09-27 17:03:36 +0200

    added run function for .json files as well as some restructuring and quality of life changes.

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ servo_map = {
     "torso": {"pin": 2, "angle": 90},
     "shoulder": {"pin": 3, "angle": 90},
     "elbow": {"pin": 4, "angle": 90},
-    "wrist": {"pin": 5, "angle": 90},
-    "hand": {"pin": 6, "angle": 90},
+    "wrist-pitch": {"pin": 5, "angle": 90},
+    "wrist-roll": {"pin": 6, "angle": 90},
+    "hand": {"pin": 7, "angle": 90},
 }
 
 def servo_move(servo, angle):
@@ -22,19 +23,39 @@ def servo_move(servo, angle):
         return
     board.digital[servo_map[servo]["pin"]].write(angle)
     servo_map[servo]["angle"] = angle
+    print(f"moving servo '{servo}' to angle '{angle}'")
 
 def save_state(data, filename):
+    """Saves current state to file."""
     state = load_state(filename)
     state.append(data)
     with open(filename, "wt") as f:
         json.dump(state, f)
+    print(f"saved '{filename}'")
 
 def load_state(filename):
-    with open(filename, "rt") as f:
-        return json.load(f)
+    """Loads state from file."""
+    try:
+        with open(filename, "rt") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
 
 def delete_file(filename):
+    """Deletes file."""
     os.remove(filename)
+    print(f"deleted '{filename}'")
+
+def run_file(filename):
+    """Runs file."""
+    commands = load_state(filename)
+    for command in commands:
+        for servo in command.keys():
+            servo_move(servo, command[servo]["angle"])
+    if len(commands) == 0:
+        print(f"no commands found in '{filename}'")
+    else:
+        print(f"successfully ran '{filename}'")
 
 def main():
     for pin in servo_map.values():
@@ -42,29 +63,42 @@ def main():
 
     while True:
         user_input = input("$ ").lower()
-        if user_input == "quit":
+        user_inputs = user_input.split()
+        command = user_inputs[0]
+        if len(user_inputs) > 1:
+            argument = user_inputs[1]
+        else:
+            argument = None
+
+        if command == "quit":
             break
 
-        elif user_input == "reset":
+        elif command == "reset":
             for pin in servo_map.values():
                 board.digital[pin["pin"]].write(90)
                 time.sleep(0.5)
 
-        elif user_input == "save":
-            data = [servo_map]
+        elif command == "save":
+            data = servo_map
             save_state(data, "data.json")
 
-        elif user_input == "delete":
+        elif command == "delete":
             delete_file("data.json")
 
-        else:
+        elif command == "run":
+            run_file(argument)
+
+        elif command in servo_map.keys():
             try:
-                input_servo, input_angle = user_input.split()
-                input_angle = int(input_angle)
-                servo_move(input_servo, input_angle)
+                if command in servo_map:
+                    argument = int(argument)
+                    servo_move(command, argument)
 
             except ValueError:
                 print(f"invalid syntax: '{user_input}'")
+
+        else:
+            print(f"invalid command: '{command}'")
 
 if __name__ == "__main__":
     main()
